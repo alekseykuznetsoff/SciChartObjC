0f52b465614699d7de5b0f7ab607205ee333cf13
Kuznetsov Aleksey <aleksey.d.kuznetsov@gmail.com>
2017-06-11 22:42:20 +0300

    Added a clean project. Created a project structure.

---
+++ SciChartObjC/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SciChartObjC
+//
+//  Created by Kuznetsov Aleksey on 11.06.17.
+//  Copyright © 2017 Kuznetsov Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SciChartObjC/Supporting Files/Macro.h
@@ -0,0 +1,13 @@
+//
+//  Macro.h
+//  SciChartObjC
+//
+//  Created by Kuznetsov Aleksey on 11.06.17.
+//  Copyright © 2017 Kuznetsov Aleksey. All rights reserved.
+//
+
+#ifndef Macro_h
+#define Macro_h
+
+
+#endif /* Macro_h */
